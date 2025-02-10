f46678d9701f14339492fa93f3540c2549f72f25
David Polívka <davidecekcz@gmail.com>
2025-02-10 15:17:19 +0100

    CV-01: Vytvoreni projektu

--- backend/src/main/java/cz/upce/fei/nnpiacv/Controller/GreetingController.java
+++ backend/src/main/java/cz/upce/fei/nnpiacv/Controller/GreetingController.java
@@ -1,11 +1,14 @@
 package cz.upce.fei.nnpiacv.Controller;
 
 import cz.upce.fei.nnpiacv.Service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+
+    @Autowired
     private GreetingService greetingService = new GreetingService();
 
     @GetMapping("/")
